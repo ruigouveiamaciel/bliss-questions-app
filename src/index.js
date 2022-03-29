22157951b1975dc41d3cabed3ad4be09f0d65e30
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-03-29 14:03:37 +0100

    Global CSS & Variables

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+import App from "./routing/App";
+import "./styles/variables.css";
+import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
