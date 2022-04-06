029cbda624bc49e618d22cb77fd0af387c51388b
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-04-06 14:14:36 +0100

    Implement function to fetch the question list

---
+++ src/api/getQuestionsList.js
@@ -0,0 +1,26 @@
+import api from "./api.js";
+
+/*
+ * Fetches the API and retrieves a list of questions.
+ *
+ * @async
+ * @param {number} limit Number of questions to fetch.
+ * @param {number} offset Number of questions to skip.
+ * @param {String|undefined} filter The search filter.
+ * @returns {Array} Array of questions according to the API specification.
+ */
+export default async function getQuestionsList(
+  limit,
+  offset = 0,
+  filter = undefined
+) {
+  const res = await api.get("/questions", {
+    params: {
+      limit,
+      offset,
+      filter,
+    },
+  });
+
+  return res.status === 200 ? res.data : false;
+}
