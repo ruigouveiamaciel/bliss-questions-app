7a7b1b4b02508f9b0dc2766c4db36f36a0abc0b0
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-04-11 20:23:43 +0100

    Radio component

---
+++ src/components/inputs/Radio/index.js
@@ -0,0 +1,25 @@
+import { useRef } from "react";
+import PropTypes from "prop-types";
+import "./styles.css";
+
+export default function Radio({ label, checked, ...props }) {
+  const ref = useRef();
+
+  return (
+    <label
+      className="radio-container"
+      onClick={() => {
+        ref.current.click();
+      }}
+    >
+      <span className="radio">
+        <span className={`radio-inner ${checked ? "checked" : ""}`}>
+          <input ref={ref} type="radio" checked={checked} {...props} />
+        </span>
+      </span>
+      <span className="radio-label">{label}</span>
+    </label>
+  );
+}
+
+Radio.propTypes = { label: PropTypes.string, checked: PropTypes.bool };
