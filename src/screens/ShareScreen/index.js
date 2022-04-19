c8b7a6f0fd5572b61cffd70f4c6dc7cd71515ea7
Rui Maciel <ruigouveiamaciel@gmail.com>
2022-04-19 15:23:54 +0100

    share screen

--- src/screens/ShareScreen/index.js
+++ src/screens/ShareScreen/index.js
@@ -1,7 +1,14 @@
-import { useLocation, Navigate } from "react-router-dom";
+import { useLocation, Navigate, useNavigate } from "react-router-dom";
+import { useState, useCallback } from "react";
+import Button from "../../components/inputs/Button";
+import TextInput from "../../components/inputs/TextInput";
+import useCancel from "../../hooks/useCancel";
+import share from "../../api/share";
+import "./styles.css";
 
 export default function ShareScreen() {
   const location = useLocation();
+  const isCancelled = useCancel();
 
   // Return to question list in case this page was open incorrectly.
   if (location.state === null) {
@@ -10,5 +17,57 @@ export default function ShareScreen() {
 
   const { from } = location.state;
 
-  return <>{from}</>;
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const onSubmit = useCallback((event) => {
+    event.preventDefault();
+
+    if (!isSubmitting) {
+      setIsSubmitting(true);
+
+      share(email, from)
+        .then((success) => {
+          if (isCancelled) return success;
+
+          if (success) {
+            navigate(-1);
+          } else {
+            setIsSubmitting(false);
+          }
+
+          return success;
+        })
+        .catch(() => {
+          if (!isCancelled) {
+            setIsSubmitting(false);
+          }
+        });
+    }
+  }, []);
+
+  return (
+    <form className="card detail-card" onSubmit={onSubmit}>
+      <label>Email</label>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChange={(event) => setEmail(event.target.value)}
+      />
+      <div className="button-container">
+        <Button type="button" error onClick={() => navigate(-1)}>
+          Cancel
+        </Button>
+        <Button
+          type="submit"
+          disabled={email.length === 0 || isSubmitting}
+          secondary
+        >
+          Share
+        </Button>
+      </div>
+    </form>
+  );
 }
